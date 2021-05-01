d487f3d7a967b18c3345ecd29e52ee16e3e47683
Kathryn Mackin <79943426+KEMackin@users.noreply.github.com>
2021-05-01 16:51:28 -0400

    Beginning Stages of Adding API Call for Daily Forecast

--- src/index.js
+++ src/index.js
@@ -20,7 +20,17 @@ function formatDate(timestamp) {
   }
   return `${day} ${hour}:${minute}`;
 }
-function displayForecast() {
+
+function getForecast(coordinates) {
+  let apiKey = "1669d85569859d438e4589e00b1b14c2";
+  let unit = "imperial";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=${unit}`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  console.log(response.data.daily);
+
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -70,6 +80,8 @@ function showTodaysWeather(response) {
   );
   localIconElement.setAttribute("alt", response.data.weather[0].description);
   dateAndTimeElement.innerHTML = formatDate(response.data.dt * 1000);
+
+  getForecast(response.data.coord);
 }
 
 function handleSubmit(event) {
@@ -124,4 +136,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 searchCity("Columbus");
-displayForecast();
